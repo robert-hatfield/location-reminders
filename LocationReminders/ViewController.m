e5e3ace1dc42169800923c1f68ef8289102c5612
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-01 11:44:00 -0700

    Import Parse framework, and test functionality

--- LocationReminders/ViewController.m
+++ LocationReminders/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import Parse;
 
 @interface ViewController ()
 
@@ -17,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//    testObject[@"testName"] = @"Robert Hatfield";
+//    [testObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"Success saving test object");
+//        } else {
+//            NSLog(@"There was a problem saving. Save error: %@", error.localizedDescription);
+//        }
+//    }];
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error when querying: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Query results:\n %@", objects);
+        }
+    }];
 }
 
 

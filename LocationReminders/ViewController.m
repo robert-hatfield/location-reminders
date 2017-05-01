cabfef12223fcb1fad272cc22eef6c10b5e31ac0
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-01 12:44:29 -0700

    Add Map view with button to zoom to a location

--- LocationReminders/ViewController.m
+++ LocationReminders/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 @import Parse;
+@import MapKit;
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,7 +21,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    [self requestPermissions];
+    self.mapView.showsUserLocation = YES;
+    
     
 //    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
 //    testObject[@"testName"] = @"Robert Hatfield";
@@ -29,16 +37,26 @@
 //        }
 //    }];
     
-    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"Error when querying: %@", error.localizedDescription);
-        } else {
-            NSLog(@"Query results:\n %@", objects);
-        }
-    }];
+//    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"Error when querying: %@", error.localizedDescription);
+//        } else {
+//            NSLog(@"Query results:\n %@", objects);
+//        }
+//    }];
 }
 
+- (void)requestPermissions {
+    self.locationManager = [[CLLocationManager alloc] init];
+    [self.locationManager requestAlwaysAuthorization];
+}
+
+- (IBAction)location1Pressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6566674, -122.351096);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+    [self.mapView setRegion:region animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
